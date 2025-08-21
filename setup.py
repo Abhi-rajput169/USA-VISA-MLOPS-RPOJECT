423943daa8fa0c9fb2bfde99b95e0289f8b2f244
Abhishek Rajput <rajputabhishek0108@gmail.com>
2025-08-21 17:49:29 +0530

    logger exception util done

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="us_visa",
+    version="0.0.0",
+    author="Abhishek",
+    author_email="rajputabhishek0108@gmail.com",
+    packages=find_packages()
+)
